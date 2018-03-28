9886acc7b97f82be3f9fed49ff7d2a9234da6870
TatsumiKei <ki3ttm@gmail.com>
2018-03-28 17:47:48 +0900

    MemberInfo他

---
+++ Assets/MemberInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemberInfo : MonoBehaviour {
+	enum MEMBER {
+		NoSelected,
+		Public,
+		Tatsumi,
+		Saito,
+		Tamura,
+		Murata,
+	}
+
+	[SerializeField] MEMBER member;	// 担当者
+	[SerializeField] string cmnt;	// 備考
+
+	// Use this for initialization
+	void Start () {
+		if (member == MEMBER.NoSelected) {
+			Debug.LogWarning("担当者が設定されていません。\n" + name);
+		}
+	}
+	
+	// Update is called once per frame
+//	void Update () {}
+}
